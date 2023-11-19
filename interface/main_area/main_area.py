74769cbf2036567ed07d12a8fdd7600d44c7427e
Martin Dawson <martin@dawson.fm>
2023-11-18 20:03:17 -0500

    Colors now working. Currently changes color of all text regardless of highlight.

--- interface/main_area/main_area.py
+++ interface/main_area/main_area.py
@@ -42,7 +42,15 @@ def toggle_text_underline():
 
     else:
         text.tag_add("underline", start_index, end_index)
-        text.tag_configure("underline", font=("Arial", current_size, "underline"))
+        if "bold" and "italic" in tags:
+            text.tag_configure("underline", font=("Arial", current_size, "underline", "bold", "italic"))
+        elif "italic" in tags:
+            text.tag_configure("underline", font=("Arial", current_size, "underline", "italic"))
+        elif "bold" in tags:
+            text.tag_configure("underline", font=("Arial", current_size, "underline", "bold"))
+        else:
+            text.tag_configure("underline", font=("Arial", current_size, "underline"))
+
 
 def toggle_text_italic():
     start_index = text.index("sel.first")
@@ -52,29 +60,36 @@ def toggle_text_italic():
     if "italic" in tags:
         text.tag_remove("italic", start_index, end_index)
         text.tag_configure("normal", font=("Arial", current_size))
-
     else:
         text.tag_add("italic", start_index, end_index)
-        text.tag_configure("italic", font=("Arial", current_size, "italic"))
+        if "bold" and "underline" in tags:
+            text.tag_configure("italic", font=("Arial", current_size, "underline", "bold", "italic"))
+        elif "underline" in tags:
+            text.tag_configure("italic", font=("Arial", current_size, "underline", "italic"))
+        elif "bold" in tags:
+            text.tag_configure("italic", font=("Arial", current_size, "italic", "bold"))
+        else:
+            text.tag_configure("italic", font=("Arial", current_size, "italic"))
 
 
 def toggle_text_bold():
     start_index = text.index("sel.first")
     end_index = text.index("sel.last")
     tags = text.tag_names(start_index)
+    current_size = text.cget("font").split(" ")[-1]
     if "bold" in tags:
         text.tag_remove("bold", start_index, end_index)
+        text.tag_configure("normal", font=("Arial", current_size))
     else:
-        font_size = text.cget("font").split(" ")[-1]
-
-        if "bold" in tags:
-            normal_font = ("TkDefaultFont", font_size)
-            text.tag_configure("normal", font=normal_font)
-            text.tag_remove("bold", start_index, end_index)
+        text.tag_add("bold", start_index, end_index)
+        if "italic" and "underline" in tags:
+            text.tag_configure("bold", font=("Arial", current_size, "underline", "bold", "italic"))
+        elif "underline" in tags:
+            text.tag_configure("bold", font=("Arial", current_size, "underline", "bold"))
+        elif "italic" in tags:
+            text.tag_configure("bold", font=("Arial", current_size, "italic", "bold"))
         else:
-            bold_font = ("TkDefaultFont", font_size, "bold")
-            text.tag_configure("bold", font=bold_font)
-            text.tag_add("bold", start_index, end_index)
+            text.tag_configure("bold", font=("Arial", current_size, "bold"))
 
 # Date Label
 date = tk.Label(header, text="", font=("Arial", 12), bg="#8a2be2", fg="white")
@@ -86,9 +101,9 @@ sidebar = tk.Frame(window, bg="#f0f0f0", width=200)
 sidebar.pack(side=tk.LEFT, fill=tk.Y)
 
 # New Note and New Folder Buttons
-new_note = tk.Button(sidebar, text="New Note", font=("Arial", 12), bg="#8a2be2", fg="#8a2be2", width=15, borderwidth=0)
+new_note = tk.Button(sidebar, text="New Note", font=("Arial", 12), highlightbackground="#f0f0f0", fg="#8a2be2", width=15, borderwidth=0)
 new_note.pack(side=tk.BOTTOM, padx=10, pady=10)
-new_folder = tk.Button(sidebar, text="New Folder", font=("Arial", 12), bg="#8a2be2", fg="#8a2be2", width=15, borderwidth=0)
+new_folder = tk.Button(sidebar, text="New Folder", font=("Arial", 12), highlightbackground="#f0f0f0", fg="#8a2be2", width=15, borderwidth=0)
 new_folder.pack(side=tk.BOTTOM, padx=10, pady=10)
 
 # List of Folders and Documents
@@ -112,40 +127,62 @@ main.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 # Formatting Buttons
 formatting = tk.Frame(main, bg="#f0f0f0", height=50)
 formatting.pack(fill=tk.X)
-options = ["Font", "Size", "Color"]
+options = ["Font", "Color"]
 
-bold_image = tk.PhotoImage(file='bold-b.pgm')
-true_bold = bold_image.subsample(30, 30)
-bold_button = tk.Button(formatting, image=true_bold, command=toggle_text_bold, borderwidth=0)
+original_bold = Image.open('bold_b.png')
+resized_bold = original_bold.resize((18,18), Image.Resampling.LANCZOS)
+bold_image = ImageTk.PhotoImage(resized_bold)
+bold_button = tk.Button(formatting, image=bold_image, command=toggle_text_bold, borderwidth=0)
 bold_button.pack(side=tk.LEFT, padx=5, pady=10)
 
-underline_image = tk.PhotoImage(file='underlined_u.pgm')
-true_underline = underline_image.subsample(55,55)
-underline_button = tk.Button(formatting, image=true_underline, borderwidth=0, command=toggle_text_underline)
+original_under = tk.PhotoImage(file='underlined_u.pgm')
+underline_image = original_under.subsample(55,55)
+underline_button = tk.Button(formatting, image=underline_image, borderwidth=0, command=toggle_text_underline)
 underline_button.pack(side=tk.LEFT, padx=5, pady=10)
 
-italic_image = tk.PhotoImage(file='italic-i.pgm')
-true_italic = italic_image.subsample(30,30)
-italic_button = tk.Button(formatting, image=true_italic, borderwidth=0, command=toggle_text_italic)
+original_italic = Image.open('italic_i.png')
+resized_italic = original_italic.resize((18,18), Image.Resampling.LANCZOS)
+italic_image = ImageTk.PhotoImage(resized_italic)
+italic_button = tk.Button(formatting, image=italic_image, borderwidth=0, command=toggle_text_italic)
 italic_button.pack(side=tk.LEFT, padx=5, pady=10)
 
 # Add code button
 original_image_pil = Image.open('add_code.png')
 resized_code = original_image_pil.resize((100,32))
 code_image = ImageTk.PhotoImage(resized_code)
-add_code_button = tk.Button(formatting, image=code_image, borderwidth=0)
+add_code_button = tk.Button(formatting, image=code_image, borderwidth=0, highlightbackground="#f0f0f0")
 add_code_button.pack(side=tk.RIGHT, padx=10, pady=10)
 
+def change_text_color(color):
+    text.config(foreground=color)
 
-for option in options:
-    button = tk.Button(formatting, text=option, font=("Arial", 12), bg="#f0f0f0", fg="black", borderwidth=0)
-    button.pack(side=tk.LEFT, padx=20, pady=10)
+# Create a list of 10 colors
+color_options = ["Black", "Red", "Green", "Blue", "Purple", "Orange", "Brown", "Pink", "Gray", "Cyan"]
+
+# Create a StringVar to store the selected color
+selected_color = tk.StringVar()
+
+# Set the default color
+selected_color.set(color_options[0])
+
+# Create an OptionMenu to choose the color
+color_menu = tk.OptionMenu(formatting, selected_color, *color_options)
+color_menu.configure(bg="#f0f0f0", fg="black", activeforeground="black")
+color_menu.pack(pady=10, side=tk.LEFT)
+
+# Create a button to apply the selected color
+apply_button = tk.Button(formatting, text="Apply Color", command=lambda: change_text_color(selected_color.get()), highlightbackground="#f0f0f0")
+apply_button.pack(pady=10, side=tk.LEFT)
+
+# for option in options:
+#     button = tk.Button(formatting, text=option, font=("Arial", 12), bg="#f0f0f0", fg="black", borderwidth=0, highlightbackground="#f0f0f0")
+#     button.pack(side=tk.LEFT, padx=20, pady=10)
 
 # Add the "+" and "-" buttons for adjusting text size
-decrease_button = tk.Button(formatting, text="-", font=("Arial", 12), bg="#f0f0f0", fg="black", command=decrease_font_size, borderwidth=0)
+decrease_button = tk.Button(formatting, text="-", font=("Arial", 12), bg="#f0f0f0", fg="black", command=decrease_font_size, borderwidth=0, highlightbackground="#f0f0f0")
 decrease_button.pack(side=tk.LEFT, pady=10)
 
-increase_button = tk.Button(formatting, text="+", font=("Arial", 12), bg="#f0f0f0", fg="black", command=increase_font_size, borderwidth=0)
+increase_button = tk.Button(formatting, text="+", font=("Arial", 12), bg="#f0f0f0", fg="black", command=increase_font_size, borderwidth=0, highlightbackground="#f0f0f0")
 increase_button.pack(side=tk.LEFT, pady=10)
 
 # Textbox
